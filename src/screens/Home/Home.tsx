d2938978b8ffd9c69b02a9e4ad28c5375a812036
gaosx <dev@gaosx.top>
2024-03-13 17:47:10 +0800

    feat: implement home screen

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+// @ts-ignore
+import bell from '../../assets/bell.png';
+import Header from '../../components/Header.tsx';
+import Background from '../../components/Background.tsx';
+import {StyleSheet} from 'react-native';
+import Balance from './components/Balance.tsx';
+import Board from './components/Board.tsx';
+import LookingFor from './components/LookingFor.tsx';
+import Card from './components/Card.tsx';
 
 export default () => {
   return (
-    <View>
-      <Text>Home Page</Text>
-    </View>
+    <Background>
+      <Header
+        rightButtonPress={() => {}}
+        title={'Boxi'}
+        rightButtonIcon={bell}
+      />
+
+      <Balance styles={styles.balance}>
+        <Board>
+          <LookingFor />
+
+          <Card />
+        </Board>
+      </Balance>
+    </Background>
   );
 };
+
+const styles = StyleSheet.create({
+  balance: {
+    marginTop: 16,
+  },
+});
