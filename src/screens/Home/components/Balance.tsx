a822783e850d2cebf364c402e0e1e88b8c919d0b
gaosx <dev@gaosx.top>
2024-03-13 14:42:55 +0800

    feat: implement balance for home screen

---
+++ src/screens/Home/components/Balance.tsx
@@ -0,0 +1,71 @@
+import React, {PropsWithChildren, useCallback, useMemo, useState} from 'react';
+import {
+  Image,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
+
+// @ts-ignore
+import balance from '../../../assets/balance.png';
+
+type Props = PropsWithChildren & {
+  styles: StyleProp<ViewStyle>;
+};
+
+export default (props: Props) => {
+  const [amount] = useState<number>(1234567.89);
+  const formattedAmount = useMemo(() => amount.toLocaleString(), [amount]);
+
+  const handlePress = useCallback(() => {}, []);
+
+  return (
+    <View style={[styles.root, props.styles]}>
+      <View style={styles.balance}>
+        <Image source={balance} style={styles.img} />
+        <Text style={styles.amountTxt} ellipsizeMode={'tail'}>
+          ${formattedAmount}
+        </Text>
+        <TouchableOpacity activeOpacity={0.7} onPress={handlePress}>
+          <Text style={styles.btnTxt}>View Boxipay</Text>
+        </TouchableOpacity>
+      </View>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    flex: 1,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: '#4B47A3',
+  },
+  balance: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    height: 56,
+    paddingHorizontal: 20,
+  },
+  img: {
+    width: 22,
+    height: 20,
+    resizeMode: 'cover',
+    tintColor: '#FAFAFA',
+  },
+  amountTxt: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 18,
+    flex: 1,
+  },
+  btnTxt: {
+    color: '#FFFFFF',
+  },
+});
